83a98c4d30c85f68ecef49f52928caf424c4616c
thomaroger <thomaroger@gmail.com>
2025-12-08 01:54:02 +0100

    fix(sql) fix migration doctrine

--- migrations/Version20251206180239.php
+++ migrations/Version20251206180239.php
@@ -19,14 +19,6 @@ final class Version20251206180239 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE assessment DROP FOREIGN KEY FK_F7523D7059027487');
-        $this->addSql('DROP INDEX IDX_F7523D7059027487 ON assessment');
-        $this->addSql('ALTER TABLE assessment CHANGE theme_id category_id INT NOT NULL');
-        $this->addSql(
-            'ALTER TABLE assessment ADD CONSTRAINT FK_F7523D7012469DE2 FOREIGN KEY (category_id) REFERENCES category (id)'
-        );
-        $this->addSql('CREATE INDEX IDX_F7523D7012469DE2 ON assessment (category_id)');
     }
 
     public function down(Schema $schema): void
